c0ff2c8aa9da0a050c81b7279c9a5d7295e06bc6
chelskeeley <chelsea.keeley@gmail.com>
2020-10-09 15:24:16 -0400

    optimize setColor

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,39 +10,25 @@ const SquareScreen = () => {
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  const setColor = (color, change) => {
-    // color === "red", "green", "blue"
-    // change === +15, -15
-    switch (color) {
-      case "red":
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-
-      case "green":
-        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-        return;
-
-      case "blue":
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-        return;
-    }
+  const setColor = (stateValue, change, setter) => {
+    stateValue + change > 255 || stateValue + change < 0 ? null : setter(stateValue + change);
   }
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setColor("red", INCREMENT)}
-        onDecrease={() => setColor("red", INCREMENT * -1)}
+        onIncrease={() => setColor(red, INCREMENT, setRed)}
+        onDecrease={() => setColor(red, INCREMENT * -1, setRed)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setColor("green", INCREMENT)}
-        onDecrease={() => setColor("green", INCREMENT * -1)}
+        onIncrease={() => setColor(green, INCREMENT, setGreen)}
+        onDecrease={() => setColor(green, INCREMENT * -1, setGreen)}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => setColor("blue", INCREMENT)}
-        onDecrease={() => setColor("blue", INCREMENT * -1)}
+        onIncrease={() => setColor(blue, INCREMENT, setBlue)}
+        onDecrease={() => setColor(blue, INCREMENT * -1, setBlue)}
         color="Blue"
       />
       <Text></Text>
